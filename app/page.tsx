d6b6ae28f49961a888914b0d292fea076228663e
Kellen Xavier <kellenxaviier@gmail.com>
2024-09-09 23:16:35 -0300

    feat: criar pagina inicial + contatos

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "Projects", href: "/projects" },
+  { name: "Projects", href: "/" },
   { name: "Contact", href: "/contact" },
 ];
 
@@ -29,20 +29,20 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        Site | Blog | Ladybug_
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
+          Em desenvolvimento {" "}
           <Link
             target="_blank"
-            href="https://unkey.dev"
+            href="/"
             className="underline duration-500 hover:text-zinc-300"
           >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+            portfolio-ladydebug.vercel.app
+          </Link> 
         </h2>
       </div>
     </div>
